1958e535045747276d5280a6c65f95467d9b457f
Paradise <zdparadise@icloud.com>
2021-01-06 18:52:51 +0800

    :smile: improvement

--- src/main/java/com/paradise/ddp/entity/BingResult.java
+++ src/main/java/com/paradise/ddp/entity/BingResult.java
@@ -4,6 +4,7 @@ package com.paradise.ddp.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  */
 @Getter
 @Setter
+@ToString
 public class BingResult {
     private List<BingImage> images;
     private String tooltips;
--- src/main/java/com/paradise/ddp/utils/BingImageUtils.java
+++ src/main/java/com/paradise/ddp/utils/BingImageUtils.java
@@ -7,6 +7,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.paradise.ddp.constant.CommonUrl;
 import com.paradise.ddp.entity.BingImage;
 import com.paradise.ddp.entity.BingResult;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -16,16 +17,29 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author dzhang
  */
+@Slf4j
 public class BingImageUtils {
 
+    private static final String BING_URL = "https://cn.bing.com/";
+    private static final String BING_IMAGE_URL = "https://cn.bing.com/HPImageArchive.aspx?format=js&pid=hp";
+
+    /**
+     * 获取 bing 图片
+     *
+     * @param index  图片索引 index；0 代表当天，1代表昨天，以此类推
+     * @param number 图片数量，最大值为 8，即返回 8张 图片地址
+     * @return {@link BingResult}
+     * @throws IOException 网络异常
+     */
     public static BingResult getBingImage(String index, String number) throws IOException {
         BingResult bingResult = new BingResult();
         HttpClient httpclient = HttpClients.createDefault();
-        StringBuilder stringBuilder = new StringBuilder(CommonUrl.BING_IMAGE_URL);
+        StringBuilder stringBuilder = new StringBuilder(BING_IMAGE_URL);
         if (StringUtils.isNotEmpty(index)) {
             stringBuilder.append("&idx=");
             stringBuilder.append(index);
@@ -43,9 +57,27 @@ public class BingImageUtils {
             String result = EntityUtils.toString(httpResponse.getEntity());
             JSONObject obj = JSONObject.parseObject(result);
             bingResult = obj.toJavaObject(BingResult.class);
-            System.out.println(bingResult.getImages().get(0).getUrl());
         }
         return bingResult;
+    }
+
+    public static void main(String[] args) {
+        try {
+            BingResult result = getBingImage("0", "8");
+            if (result != null) {
+                List<BingImage> images = result.getImages();
+                int x = 0;
+                for (BingImage image : images) {
+                    log.info("序号：{}", x);
+                    log.info("name:{}", image.getCopyright());
+                    log.info("url:{}", image.getUrl());
+                    log.info("{}", image);
+                    x++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Message bingResult2Msg(BingImage image) {
